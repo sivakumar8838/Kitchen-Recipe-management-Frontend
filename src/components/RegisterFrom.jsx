d0336809ce187341cd7230ee4c0213d0d6566433
siva <sivamss5882@gmail.com>
2023-12-20 20:19:57 +0530

    hi

--- src/components/RegisterFrom.jsx
+++ src/components/RegisterFrom.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import "./Register.css"
+import "../Register.css"
 
 function RegisterFrom({handleRegister, register, setRegister}){
     return(
