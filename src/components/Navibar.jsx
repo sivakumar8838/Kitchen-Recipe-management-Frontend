2088c1bbb6451f471bcf108d8cd7318b0cb876e6
siva <sivamss5882@gmail.com>
2023-12-23 16:24:39 +0530

    hi

---
+++ src/components/Navibar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navibar() {
+
+  return (
+    <div>
+    <Home />
+  <Chicken />
+  <Biryani />
+  <Veg />
+  <Paneer />
+
+    </div>
+  )
+}
+
+export default Navibar
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,63 +1,62 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import './Recipes.css'
 
 function Recipes() {
   const [recipe, setRecipe] = useState([]);
 
   const currentUrl = window.location.href;
 
-  // const handlecilk = async (e) => {
-  //   e.preventDefault();
-
-  //   const slicedUrl = currentUrl.slice(-24);
-
-  //   const response = await axios.get(`https://kitchen-recipe-management-agpb.onrender.com/api/Recipe/${slicedUrl}`);
-  //   setRecipe(response.data);
-  //   console.log(response.data);
-
-  // };
+  const handlecilk = async (e) => {
+    if (e) {
+      e.preventDefault();
+    }
   
-
-  useEffect(() =>{
-    const handlecilk = async () => {
-      const slicedUrl = currentUrl.slice(-24);
-    
+    const slicedUrl = currentUrl.slice(-24);
+  
     try {
       const response = await axios.get(`https://kitchen-recipe-management-agpb.onrender.com/api/Recipe/${slicedUrl}`);
       setRecipe(response.data);
-      console.log(response.data);
-  
-      
     } catch (error) {
       console.error(error);
     }
   };
+  
 
-  handlecilk();
-    
-  }, [])
+  useEffect(() => {
+    handlecilk();
+  }, []);
 
   return (
     <div>
-      {/* <button onClick={handlecilk}>cilk</button> */}
       <div className="recipe-container">
-      {Array.isArray(recipe) && recipe.map((recipe) => (
-  <section className="card5">
-    <img className='img' height={200} width={250} src={recipe.img} alt={recipe.name} />
-    <div className="card5__content">
-      <p className="card5__title">{recipe.name}</p>
-      <p className="card5__description">{recipe.description}</p>
-      <ul>
-        {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-    </div>
-  </section>
-))}
+         
+            <section className='card6'>
+            <h1>{recipe.name}</h1>
 
-</div>
+              <img alt={recipe.name} src={recipe.img} className='img2'/>
+              <div >
+                <h2> category</h2><p>{recipe.category}</p>
+                <h2>description</h2>
 
+                <p>{recipe.description}</p>
+                <h2>ingredients </h2>
+                <p>
+                {recipe.ingredients}</p>
+                <h2>instructions</h2>
+                <p> {recipe.instructions} </p>
+              </div>
+              {/* <div className="recipe-container">
+          {home.map((recipe) => (
+            <section key={recipe._id} className="card5" onClick={() => handleRecipes(recipe._id)}>
+              <img className='img' height={200} width={250} src={recipe.img} alt={recipe.name} />
+              <p className='p'>{recipe.name}</p>
+              <div className="card5__content">
+                <p className="card5__title">{recipe.name}</p>
+                <p className="card5__description">{recipe.description}</p>
+              </div> */}
+            </section>
+        </div>
     </div>
   );
 }
